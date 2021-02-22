82505976d59731a4920cf6642f0609d68aedfc91
LorriAnn <LorriAnn7@att.net>
2021-02-22 11:42:05 -0500

    creates home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import '../styles/App.css';
+import '../styles/home.css';
 
-const Home = () => (
-    <div>
-      <h1>Home Page</h1>
+const Home = () => {
+
+  return (
+    <div className = 'background'>
+      <h1>George C. Parker's 
+          <br/>
+          Quality Bridges
+      </h1>
+      <h3>"Every path in life has a bridge ..."</h3>
+      <button>Shop Now</button>
     </div>
-  )
+  );
+}
 
   export default Home;
